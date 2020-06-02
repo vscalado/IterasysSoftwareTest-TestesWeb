3e5d43bd45e5b1536134a734600e26b2b759e81a
vscalado <vitor.santos.calado@gmail.com>
2020-06-02 20:26:53 -0300

    Criação do Repositório de Testes Web. Ultima Aula:Confirmar que o Carrinho de Compras está Vazio

---
+++ ecommerce_prestashop/src/main/java/pages/HomePage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+	
+	private WebDriver driver;
+	List<WebElement> listaProdutos = new ArrayList();
+	
+	private By produtos = By.className("product-description");
+	private By textoProdutosNoCarrinho = By.className("cart-products-count");
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public int contarProdutos() {
+		carregaListaProdutos();
+		return listaProdutos.size();
+	}
+	
+	private void carregaListaProdutos() {
+		listaProdutos = driver.findElements(produtos);
+	}
+	
+	public int obterQuantidadeProdutosNoCarrinho() {
+		
+		String quantidadeProdutosNoCarrinho = driver.findElement(textoProdutosNoCarrinho).getText();
+		
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace("(", "");
+		quantidadeProdutosNoCarrinho = quantidadeProdutosNoCarrinho.replace(")", "");
+		
+		int qtdProdutosNoCarrinho = Integer.parseInt(quantidadeProdutosNoCarrinho);
+		
+		return qtdProdutosNoCarrinho;
+	}
+
+}
